cea3fdab67df78fba8fa59830b6d16bbcf40256e
ravel <sebastien.ravel@cirad.fr>
2020-07-10 15:22:58 +0200

    rm extention for readthedoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -220,9 +220,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-  ('index'),
-]
+
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -250,7 +248,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index')
+
 ]
 
 # If true, show URL addresses after external links.
@@ -263,7 +261,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index'),
+
 ]
 
 # Documents to append as an appendix to all manuals.
