6332517159361565dedf2926ab618b3bab5001a7
ravel <sebastien.ravel@cirad.fr>
2020-07-10 15:25:40 +0200

    rm extention for readthedoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ extensions = ['sphinx.ext.intersphinx',
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = ['.rst']
+source_suffix = ['.rst', "md"]
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
