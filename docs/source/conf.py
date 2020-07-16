e0cff3765dded83475af0f42843a3b25b5cfe659
ravel <sebastien.ravel@cirad.fr>
2020-07-16 10:54:59 +0200

    add latex doc build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,11 @@ copyright = '2019-2020, Julie Orjuela (IRD), Aurore Comte (IRD), Sebastien Ravel
 github_doc_root = 'https://github.com/SouthGreenPlatform/CulebrONT_pipeline/tree/master/docs/'
 issues_github_path = 'https://github.com/SouthGreenPlatform/CulebrONT_pipeline/issues'
 
+latex_authors = '''
+Julie Orjuela (IRD),\\\\
+Aurore Comte (IRD),\\\\
+Sebastien Ravel (CIRAD),\\\\
+Florian Charriat (CIRAD)'''
 
 
 # import culebront
@@ -219,30 +224,50 @@ htmlhelp_basename = 'CulebrONTdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
+latex_engine = 'pdflatex'
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    'papersize': 'a4paper',
+    # The font size ('10pt', '11pt' or '12pt').
+    'pointsize': '12pt',
+    # Latex figure (float) alignment
+    'figure_align':'htbp',
+    'extraclassoptions': 'openany',
+    'preamble': r'''
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%add number to subsubsection 2=subsection, 3=subsubsection
+        \setcounter{secnumdepth}{0}
+        %%%% Table of content upto 2=subsection, 3=subsubsection
+        \setcounter{tocdepth}{2}
+    ''',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    'sphinxsetup': \
+        'hmargin={0.7in,0.7in}, vmargin={0.7in,0.7in}, \
+        marginpar=1in, \
+        verbatimwithframe=False, \
+        TitleColor={rgb}{0,0,0}, \
+        HeaderFamily=\\rmfamily\\bfseries, \
+        InnerLinkColor={rgb}{0,0,1}, \
+        OuterLinkColor={rgb}{0,0,1}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 
+latex_documents = [
+  ('index', 'CulebrONT.tex', 'Documentation',
+   latex_authors, 'manual', True),
+]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = './SupplementaryFiles/culebront_logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+latex_use_parts = False
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
@@ -256,6 +281,7 @@ latex_elements = {
 # If false, no module index is generated.
 #latex_domain_indices = True
 
+latex_toplevel_sectioning = 'section'
 
 # -- Options for manual page output ---------------------------------------
 
