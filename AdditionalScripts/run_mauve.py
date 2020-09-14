8cd254c57b9949d5ee50cd42b4fc4e11b13ed2fc
orjuela <julie.orjuela@ird.fr>
2020-09-14 15:09:41 +0200

    tranfert from gitlab, fixing bug of mauve

--- AdditionalScripts/run_mauve.py
+++ AdditionalScripts/run_mauve.py
@@ -12,43 +12,39 @@ import subprocess
 fasta_files = snakemake.input.liste
 out_dir = snakemake.params.out_dir
 xmfa = snakemake.output.xmfa
-circular = snakemake.params.circular
-dir_quast = snakemake.params.dir_quast
+dir_aggregated = snakemake.params.dir_aggregated
 log_e = snakemake.log.error
 log_o = snakemake.log.output
 
-if circular:
-    common_prefix = os.path.commonprefix(fasta_files)  # find common path
-    reduced_paths = [re.sub(f"({common_prefix})", "", path) for path in fasta_files]  # gsub common path to remove it from all inputFiles
-    dest_paths = []
-    for path in reduced_paths:
-        fields = path.split("/")[0:-1]  # split and remove common leave (startfixed_asm.fasta)
-        fields.append("asm.fasta")
-        label = '-'.join(fields)
-        path = os.path.join(out_dir, label)
-        dest_paths.append(path)
+# find common path of fasta files
+common_prefix = os.path.commonprefix(fasta_files)
+reduced_paths = [re.sub(f"({common_prefix})", "", path) for path in fasta_files]  # gsub common path to remove it from all inputFiles
 
-    for src, dst in zip(fasta_files, dest_paths):  # symlink the fasta files in outDir to have them if need be
-        if os.path.isfile(dst):
-            os.remove(dst)
-        os.symlink(src, dst)
+dest_paths = []
+# remove symbolic links from another run
+for path in reduced_paths:
+    fields = path.split("/")[0:-1]  # split and remove common leave (startfixed_asm.fasta)
+    fields.append("asm.fasta")
+    label = '-'.join(fields)
+    path = os.path.join(out_dir, label)
+    dest_paths.append(path)
 
-    cmd = ["progressiveMauve", f"--output={xmfa}"] + dest_paths
-else:
-    cmd = ["progressiveMauve", f"--output={xmfa}"] + fasta_files
+# recovery path of renamed symbolic files
+for src, dst in zip(fasta_files, dest_paths):
+    if os.path.isfile(dst):
+        os.remove(dst)
+    os.symlink(src, dst)
+stringList = ' '.join([str(item) for item in dest_paths])
+
+# convert list on string
+cmd = f"progressiveMauve --output={xmfa} {stringList}"
 
 #launching MSA
-mauve_out = subprocess.run(cmd, check=True, capture_output=False, text=True, stdout=open(log_o, 'w'), stderr=open(log_e, 'w'))
-print("***", mauve_out)
+#os.system(cmd)
+subprocess.run(cmd, shell=True, capture_output=False, stdout=open(log_o, 'w'), stderr=open(log_e, 'w'))
 
 #rename fasta on xmfa
-subst = f's\'|{dir_quast}||\'ig' if not circular else f's\'|{out_dir}||\'ig'
-# print('**SB**', circular, subst)
+subst = f's\'|{dir_aggregated}||\'ig'
 cmd = f"sed -i {subst} {xmfa}"
 #os.system(cmd)
-print('**CMD**', cmd)
-
-#cmd = ["sed", "-i", subst, xmfa]
-#mauve_renaming = subprocess.run(cmd, check=True, capture_output=True, text=True)
-#print(mauve_renaming.stdout)
-#print(mauve_renaming.stderr)
+subprocess.run(cmd, shell=True, capture_output=False)
