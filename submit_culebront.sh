fb39398ec3d2770751cef4733dbb4879394d59b3
orjuela <julie.orjuela@ird.fr>
2021-09-08 17:20:28 +0200

    binding singularity

--- submit_culebront.sh
+++ submit_culebront.sh
@@ -102,7 +102,7 @@ elif [[ ! $profile ]] && [[ ! $cluster_config ]]; then
   $additional"
   snakemake -p -s ${CULEBRONT}/Snakefile \
   --configfile ${config} \
-  --use-singularity \
+  --use-singularity --singularity-args "--bind ${HOME}" \
   ${additional}
 
 # cluster_config T, profile F
