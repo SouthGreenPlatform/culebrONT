d3a44265a4c0508d00af781cfef988577c140193
orjuela <julie.orjuela@ird.fr>
2022-02-22 14:23:01 +0100

    fix tag pypi

--- setup.py
+++ setup.py
@@ -11,16 +11,11 @@ site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 NAME = 'culebrONT'
 URL = 'https://github.com/SouthGreenPlatform/culebrONT'
 CURRENT_PATH = Path(__file__).resolve().parent
-VERSION = "2.0.0b3"
+VERSION = "2.0.0"
 
-__doc__ = """Today, assembly a genome using long reads from Oxford Nanopore Technologies is really interesting in 
-particular to solve repeats and structural variants in prokaryotic as well as in eukaryotic genomes. Assemblies are 
-increasing contiguity and accuracy. The daily increase of data sequences obtained and the fact that more and more 
-tools are being released or updated every week, many species are having their genomes assembled and that’s is great … 
-“But which assembly tool could give the best results for your favorite organism?” CulebrONT can help you! CulebrONT 
-is an open-source, scalable, modular and traceable Snakemake pipeline, able to launch multiple assembly tools in 
-parallel, giving you the possibility of circularise, polish, and correct assemblies, checking quality. CulebrONT can 
-help to choose the best assembly between all possibilities. """
+__doc__ = """CulebrONT is an open-source, scalable, modular and traceable Snakemake pipeline, able to launch multiple
+assembly tools in parallel, giving you the possibility of circularise, polish, and correct assemblies, checking quality.
+CulebrONT can help to choose the best assembly between all possibilities. """
 
 
 
